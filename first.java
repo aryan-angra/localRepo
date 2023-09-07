b4f812a8eb520de367a57ad844230f9b5e574b30
aryan-angra <aryanangras@gmail.com>
2023-09-07 22:31:53 +0530

    new cd var added

--- first.java
+++ first.java
@@ -2,6 +2,7 @@ class  A{
     public static void main(String args[]){
         int a=10;
         char c= 'ab';
+        char d="cd"
         System.out.println("Hello");
         // basic program
 
